55ef3d3fca47576f362a98cacb1aeb69d827a8d9
Langelozzi <lucasangelozzi32@gmail.com>
2022-04-23 20:41:12 -0700

    Added docstrings and comments

--- rename.py
+++ rename.py
@@ -1,14 +1,22 @@
 # Author: Lucas Angelozzi
 # Date: 04/23/22
-# Purpose: Script to change file names with spaces to underscores
+# Purpose: Script to change keywords/characters in filenames and directory names recursively
 
 # Imports
 import os
 import sys
 
 
-def replace_spaces(path: str, file: str) -> str:
+def replace_char(path: str, file: str):
+    """Replaces the old string with the new string
+
+    Args:
+        path (str): the path to the file/dir
+        file (str): the name of the file/dir
+    """
+    
     f_name, f_ext = os.path.splitext(file)
+    # The old and new strings passed in as cmd arguments
     old = str(sys.argv[2])
     new = str(sys.argv[3])
     
@@ -19,19 +27,25 @@ def replace_spaces(path: str, file: str) -> str:
         print(f"{file} --> {new_name}")
 
 
-def loop_files(start_path):
-    path = start_path
+def loop_files(start_path: str):
+    """Loops through subdirectories and files at a path and calls
+    the replace function on each one
 
-    for path, subdirs, files in os.walk(path):
+    Args:
+        start_path (str): the path from which you want to start (root path)
+    """
+
+    for path, subdirs, files in os.walk(start_path):
         for dir in subdirs:
-            replace_spaces(path, dir)
-    for path, subdirs, files in os.walk(path):
+            replace_char(path, dir)
+    for path, subdirs, files in os.walk(start_path):
         for name in files:
-            replace_spaces(path, name)
+            replace_char(path, name)
 
 
 def main():
-    starting_path = sys.argv[1]
+    # The root path passed in as the first cmd argument after the script
+    starting_path = str(sys.argv[1])
     for i in range(0, 100):
         loop_files(starting_path)
 
